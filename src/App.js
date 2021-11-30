e6ca051940e400d6ec0e50ed152e133dd5fa80d6
Amritpal Singh <AmritMaan2221@gmail.com>
2021-11-30 14:05:22 -0500

    done with front page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
 import './App.css';
 import Header from './Components/Header';
 import Home from './Components/Home';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+
+        <Routes>
+          <Route path="/"
+            element={
+              <>
+                <Header />
+                <Home />
+              </>
+            }
+          />
+          <Route path="/checkout"
+            element={
+              <>
+                <Header />
+                <h1>I am checkout</h1>
+              </>
+            }
+          />
+
+
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
